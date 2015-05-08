ab8437d71157d0b1fb6ad6be3c04a0bdfb5aed30
xavwes <xaver.westenthanner@googlemail.com>
2015-05-08 13:40:52 +0200

    list view for search

--- app/src/main/java/de/xwes/meinverein/main/view/MainActivity.java
+++ app/src/main/java/de/xwes/meinverein/main/view/MainActivity.java
@@ -1,8 +1,8 @@
 package de.xwes.meinverein.main.view;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +16,7 @@ import de.xwes.meinverein.R;
 import de.xwes.meinverein.main.service.request.SearchRequest;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends Activity
 {
     EditText search_input;
 
--- app/src/main/java/de/xwes/meinverein/main/view/SearchResultsActivity.java
+++ app/src/main/java/de/xwes/meinverein/main/view/SearchResultsActivity.java
@@ -1,8 +1,11 @@
 package de.xwes.meinverein.main.view;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -12,27 +15,43 @@ import org.json.JSONObject;
 import de.xwes.meinverein.R;
 
 
-public class SearchResultsActivity extends ActionBarActivity {
+public class SearchResultsActivity extends Activity
+{
+
+    private String[] teams;
+    private String[] links;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
-        TextView teamname = (TextView) findViewById(R.id.teamname);
+
+        ListView list = (ListView) findViewById(R.id.search_results);
 
         Intent intent = getIntent();
 
         String json = intent.getStringExtra("json");
         try {
             JSONArray jObj = new JSONArray(json);
+            teams = new String[jObj.length()];
+            links = new String[jObj.length()];
             for(int i = 0; i < jObj.length(); i++)
             {
                 JSONObject jsonObject = jObj.getJSONObject(i);
-                teamname.setText(jsonObject.getString("teamname"));
+                teams[i] = jObj.getJSONObject(i).getString("teamname");
+                links[i] = jObj.getJSONObject(i).getString("teamlink");
             }
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.search_item, R.id.teamname, teams);
+        list.setAdapter(adapter);
+
+        for(int j = 0; j < teams.length; j++)
+        {
+            Log.i("Team " + j, links[j]);
         }
 
     }
