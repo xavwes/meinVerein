dffd7665c95f465d4571c32def15ce7db2c7b80c
xavwes <xaver.westenthanner@googlemail.com>
2015-06-01 14:42:32 +0200

    synchronize Service

--- server/synchronizeData.php
+++ server/synchronizeData.php
@@ -11,22 +11,31 @@
     $connection = connectToDatabase();
 
     $teamname = $_GET['name'];
+    $json = array();
+    $spiele = array();
+    $teams = array();
 
     $sql = "Select * from " . $teamname;
     $result = mysql_query($sql);
 
-    if(mysql_num_rows($result) > 0)
+    while($row = mysql_fetch_array($result))
     {
-
+        $teamarray = array("team" => $row["mannschaft"]);
+        array_push($teams,$teamarray);
     }
 
     $sql = "Select * from spiele_" . $teamname;
     $result = mysql_query($sql);
 
-    if(mysql_num_rows($result) > 0)
+    while($row = mysql_fetch_array($result))
     {
-
+        $gamearray = array("home" => $row["home"], "away" => $row["away"], "ergebnis" => $row["ergebnis"], "ort" => $row["ort"], "zeit" => $row["zeit"]);
+        array_push($spiele, $gamearray);
     }
 
+    array_push($json, $teams);
+    array_push($json, $spiele);
+
+    echo json_encode($json);
 
 ?>
